d5fe21b10da5c29b384ee4c936ddcae446d5399c
Aigio Liu <lysoft.pw@foxmail.com>
2022-02-16 11:28:52 +0800

    Add TFM net40

---
+++ WinDivertSharp/Compat/System.Buffers.Binary.BinaryPrimitives.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// https://github.com/dotnet/runtime/blob/v6.0.2/src/libraries/System.Private.CoreLib/src/System/Buffers/Binary/Reader.cs#L61
+
+#if NET40
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace System.Buffers.Binary
+{
+    internal static class BinaryPrimitives
+    {
+        [MethodImpl(256)]
+        public static ushort ReverseEndianness(ushort value)
+        {
+            // Don't need to AND with 0xFF00 or 0x00FF since the final
+            // cast back to ushort will clear out all bits above [ 15 .. 00 ].
+            // This is normally implemented via "movzx eax, ax" on the return.
+            // Alternatively, the compiler could elide the movzx instruction
+            // entirely if it knows the caller is only going to access "ax"
+            // instead of "eax" / "rax" when the function returns.
+
+            return (ushort)((value >> 8) + (value << 8));
+        }
+
+        [MethodImpl(256)]
+        public static uint ReverseEndianness(uint value)
+        {
+            // This takes advantage of the fact that the JIT can detect
+            // ROL32 / ROR32 patterns and output the correct intrinsic.
+            //
+            // Input: value = [ ww xx yy zz ]
+            //
+            // First line generates : [ ww xx yy zz ]
+            //                      & [ 00 FF 00 FF ]
+            //                      = [ 00 xx 00 zz ]
+            //             ROR32(8) = [ zz 00 xx 00 ]
+            //
+            // Second line generates: [ ww xx yy zz ]
+            //                      & [ FF 00 FF 00 ]
+            //                      = [ ww 00 yy 00 ]
+            //             ROL32(8) = [ 00 yy 00 ww ]
+            //
+            //                (sum) = [ zz yy xx ww ]
+            //
+            // Testing shows that throughput increases if the AND
+            // is performed before the ROL / ROR.
+
+            return BitOperations.RotateRight(value & 0x00FF00FFu, 8) // xx zz
+                + BitOperations.RotateLeft(value & 0xFF00FF00u, 8); // ww yy
+        }
+    }
+}
+#endif
